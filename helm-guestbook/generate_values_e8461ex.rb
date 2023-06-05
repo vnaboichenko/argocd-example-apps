5ea0b7c38e147f3d057be6e59956232cf3d2d589
Vladyslav Naboichenko <vladn@meraki.com>
2023-06-05 16:50:29 -0700

    Set 8

--- helm-guestbook/generate_values_e8461ex.rb
+++ helm-guestbook/generate_values_e8461ex.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
-replicas=rand.to_s[2..2]
+#replicas=rand.to_s[2..2]
 
-`echo "replicaCount: #{replicas}" > dynamic-values-vladn.yaml`
+#`echo "replicaCount: #{replicas}" > dynamic-values-vladn.yaml`
+`echo "replicaCount: 8" > dynamic-values-vladn.yaml`
